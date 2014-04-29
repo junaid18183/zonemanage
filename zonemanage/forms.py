75bc29ac093093c59dbca07f6c047e65cfe40685
Juned Memon <junaid18183@gmail.com>
2014-04-29 08:48:58 -0700

    Fixed the templates

--- zonemanage/forms.py
+++ zonemanage/forms.py
@@ -3,8 +3,9 @@ from django import forms
 class SOAForm(forms.Form):
     mname = forms.CharField(label='Master Name Server')
     rname = forms.CharField(label='Admin Email')
-    serial = forms.IntegerField(widget=forms.NumberInput(attrs={'disabled':'True'}))
+    #serial = forms.IntegerField(widget=forms.NumberInput(attrs={'disabled':'True'}))
     #serial = forms.IntegerField(widget=forms.NumberInput(attrs={'readonly':'True'}))
+    serial = forms.IntegerField(widget=forms.NumberInput)
     refresh = forms.IntegerField()
     retry = forms.IntegerField()
     expire = forms.IntegerField()
--- zonemanage/views.py
+++ zonemanage/views.py
@@ -5,11 +5,12 @@ from forms import *
 
 def home(request):
 	""" List the main index page for ZoneManage """
-	return render(request, "index.htm")
+	msg=["Welcome to Zonemanage"]
+	return render(request, "index.htm" , {'data' : msg})
 
 def view_zone_list(request):
 	""" List the main index page for ZoneManage """
-	#zone_array = [f for f in os.listdir(zonedir) if len(f) > 2 and f[0] not in ('.', '_')]
+	zone_array = [f for f in os.listdir(zonedir) if len(f) > 2 and f[0] not in ('.', '_')]
 	zone_array = [f for f in os.listdir(zonedir) if fnmatch.fnmatch(f,'*.arpa') or fnmatch.fnmatch(f,'glam.*')]
 	return render(request, "list_server_zones.htm",{ "zone_array" : zone_array})
 
@@ -22,27 +23,23 @@ def view_zone_detail(request,zonename):
 	zone_data=dns_records(z,hostnames)
 	return render(request, "zone_data.htm", { "zone_name" : zonename,"zone_data" : zone_data,"soa_fields" : soa_fields})
 
-
 def edit_soa(request,zonename):
-	"""Edit the SOA details of the zone"""
-	z=get_zone(zonename)
-	soa_fields=soa_detail(z)
-	return render(request, "edit_soa.htm", { "zone_name" : zonename,"soa_fields" : soa_fields})
-
-def edit_soa1(request,zonename):
 	"""Edit the SOA details of the zone"""
 	z=get_zone(zonename)
 	soa_fields=soa_detail(z)
     	if request.method == 'POST': # If the form has been submitted...
         	form = SOAForm(request.POST) # A form bound to the POST data
 	        if form.is_valid(): # All validation rules pass
-	            # Process the data in form.cleaned_data
-	            # ...
-	            return HttpResponseRedirect('/') # Redirect after POST
+                        soa_fields = form.cleaned_data
+                        status=savesoa(zonename,soa_fields)
+			msg=[zonename + " saved Successfully. However you need to reload the RNDC to make it effective."]
+			A='/reloadzone/'+zonename
+			URL='Reload Zone'
+                        return render(request, "index.htm" , {"data" : msg , 'URL' : URL ,'A' : A } )
 	else:
 	        form = SOAForm(initial=soa_fields) # An unbound form
 
-	return render(request, 'edit_soa1.htm', {'form': form,"zone_name" : zonename})
+	return render(request, 'edit_soa.htm', {'form': form,"zone_name" : zonename})
 
 
 def edit_zone(request,zonename):
@@ -72,15 +69,6 @@ def edit_zone1(request,zonename):
 	return render(request, "edit_zone1.htm", { "zone_name" : zonename,"form" : form })
 
 
-def save_soa(request,zonename):
-	"""Save the SOA details of the zone"""
-	if request.method == 'POST':
-        	form = SOAForm(data=request.POST)
-		if form.is_valid():
-			soa_fields = form.cleaned_data
-			status=savesoa(zonename,soa_fields)
-	return render(request, "save_soa.htm" , {"zonename" : zonename } )
-
 def save_zone(request,zonename):
 	"""Save the Zone details of the zone"""
 	return render(request, "save_zone.htm" )
@@ -88,5 +76,5 @@ def save_zone(request,zonename):
 def reload_zone(request,zonename):
 	"""Save the Zone details of the zone"""
 	status=reload(zonename)
-	data=status+"\n Reload of zone "+zonename+"Successfull"
+	data=[status,"Reload of zone "+zonename+" Successfull"]
 	return render(request, "index.htm",{"data" : data } )
